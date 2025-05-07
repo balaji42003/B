7ab2a09da05d73bbcbcc22ddd9dbfbf3901569b2
Balaji42003 <gandalabalaji@gmail.com>
2025-05-07 18:48:25 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/B/',
+  base: process.env.NODE_ENV === 'production' ? '/B/' : '/',
   plugins: [react()],
 })
