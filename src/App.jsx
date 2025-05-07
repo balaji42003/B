2f6116ca0415f9afee4d31cd54b0939c22e0d747
Balaji42003 <gandalabalaji@gmail.com>
2025-05-07 19:08:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
         <Navbar />
         <main className="main-content">
           <Routes>
-            <Route path="/" element={
+            <Route path="/B/" element={
               <>
                 <Hero />
                 <About />
